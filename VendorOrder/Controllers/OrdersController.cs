a73866b28b64a1e9b8b1b8471a60fde44b7493e8
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-17 12:24:39 -0800

    Add Share > _Layout.cshtml file

--- VendorOrder/Controllers/OrdersController.cs
+++ VendorOrder/Controllers/OrdersController.cs
@@ -7,22 +7,22 @@ namespace VendorOrder.Controllers
   public class OrderController : Controller
   {
 
-    [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
-    {
-      Vendor Vendor = Vendor.Find(vendorId);
-      return View(Vendor);
-    }
+    // [HttpGet("/vendors/{vendorId}/orders/new")]
+    // public ActionResult New(int vendorId)
+    // {
+    //   Vendor Vendor = Vendor.Find(vendorId);
+    //   return View(Vendor);
+    // }
 
-    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
-    public ActionResult Show(int vendorId, int orderId)
-    {
-      Order foundOrder = Order.Find(orderId);
-      Vendor Vendor = Vendor.Find(vendorId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("album", foundOrder);
-      model.Add("artist", Vendor);
-      return View(model);
-    }
+    // [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    // public ActionResult Show(int vendorId, int orderId)
+    // {
+    //   Order foundOrder = Order.Find(orderId);
+    //   Vendor Vendor = Vendor.Find(vendorId);
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   model.Add("album", foundOrder);
+    //   model.Add("artist", Vendor);
+    //   return View(model);
+    // }
   }
 }
